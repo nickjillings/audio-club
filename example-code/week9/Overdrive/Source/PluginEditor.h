45723acb260b389ce59e0fc0a91483838d2ad45d
Ryan Stables <ryan.stables@bcu.ac.uk>
2017-02-22 13:55:01 +0000

    week9 added

---
+++ example-code/week9/Overdrive/Source/PluginEditor.h
@@ -0,0 +1,34 @@
+
+
+#ifndef PLUGINEDITOR_H_INCLUDED
+#define PLUGINEDITOR_H_INCLUDED
+
+#include "../JuceLibraryCode/JuceHeader.h"
+#include "PluginProcessor.h"
+
+
+class OverdriveAudioProcessorEditor  : public AudioProcessorEditor,
+                                       public Slider::Listener,
+                                       private Timer
+{
+public:
+    OverdriveAudioProcessorEditor (OverdriveAudioProcessor&);
+    ~OverdriveAudioProcessorEditor();
+
+    void paint (Graphics&) override;
+    void resized() override;
+    
+    void sliderValueChanged (Slider *slider) override;
+    void timerCallback() override;
+    
+
+private:
+    OverdriveAudioProcessor& processor;
+    Slider threshSlider, inputGainSlider, outputGainSlider, freqSlider;
+    
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (OverdriveAudioProcessorEditor)
+};
+
+
+#endif  // PLUGINEDITOR_H_INCLUDED
+
